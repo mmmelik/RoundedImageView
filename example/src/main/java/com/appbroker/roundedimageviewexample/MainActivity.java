9e0d01929d831b93dbaf79b276e35edf21cc3090
Melik <melikozy@gmail.com>
2021-05-18 23:51:15 +0300

    example

--- example/src/main/java/com/appbroker/roundedimageviewexample/MainActivity.java
+++ example/src/main/java/com/appbroker/roundedimageviewexample/MainActivity.java
@@ -6,13 +6,13 @@ import android.os.Bundle;
 import com.appbroker.roundedimageview.RoundedImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private RoundedImageView roundedImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        roundedImageView=findViewById(R.id.roundedImageView);
-        roundedImageView.setRadius(5);
-        roundedImageView.setRadius(RoundedImageView.Corner.BOTTOM_LEFT,50);
+        RoundedImageView roundedImageView=findViewById(R.id.roundedImageView);
+        roundedImageView.setRadius(5); //Sets radius of all corners
+        roundedImageView.setRadius(RoundedImageView.Corner.BOTTOM_LEFT,50); //Override BOTTOM_LEFT corner.
+        roundedImageView.setImageDrawable(getResources().getDrawable(R.drawable.free)); //First set radius then set drawable.
     }
 }
